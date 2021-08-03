d85b9af150dda7ce60b58339af20e6c95f790f43
Anton Gushcha <ncrashed@protonmail.com>
2021-08-03 12:57:15 +0300

    Implement transaction blocking for 5 minutes

--- multisig-service/src/main.rs
+++ multisig-service/src/main.rs
@@ -18,9 +18,42 @@ use rocket::fairing::AdHoc;
 use rocket::form::Form;
 use rocket::fs::{relative, FileServer};
 use rocket::response::Redirect;
-use rocket::{Build, Rocket};
+use rocket::{Build, Rocket, State};
+use rocket::http::{CookieJar, Cookie};
+use rocket::serde::{Serialize, json::Json};
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+use chrono::{NaiveDateTime, Utc, Duration};
 
 use montelibero_transactions::transaction::validate_mtl_tx;
+
+struct Cache {
+    blocks: Arc<Mutex<HashMap<Vec<u8>, NaiveDateTime>>>,
+}
+
+impl Cache {
+    fn new() -> Self {
+        Cache {
+            blocks: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    fn is_blocked(&self, tid: &[u8]) -> bool {
+        if let Some(t) = self.blocks.lock().unwrap().get(tid) {
+            *t > Utc::now().naive_utc()
+        } else {
+            false
+        }
+    }
+
+    fn block(&self, tid: &[u8], delay: Duration) {
+        self.blocks.lock().unwrap().insert(tid.to_owned(), Utc::now().naive_utc() + delay);
+    }
+
+    fn unblock(&self, tid: &[u8]) {
+        self.blocks.lock().unwrap().remove(tid);
+    }
+}
 
 #[get("/")]
 pub fn index() -> Redirect {
@@ -38,7 +71,7 @@ pub enum ViewError {
 }
 
 #[get("/view?<tid>")]
-async fn view_transaction(conn: TransactionsDb, tid: Option<String>) -> Template {
+async fn view_transaction(conn: TransactionsDb, cache: &State<Cache>, cookies: &CookieJar<'_>, tid: Option<String>) -> Template {
 
     fn render_error(err_message: &str) -> Template {
         Template::render(
@@ -53,12 +86,18 @@ async fn view_transaction(conn: TransactionsDb, tid: Option<String>) -> Template
         )
     }
 
-    async fn view(conn: TransactionsDb, mtid: Option<String>) -> Result<Template, ViewError> {
+    async fn view(conn: TransactionsDb, cache: &State<Cache>, cookies: &CookieJar<'_>, mtid: Option<String>) -> Result<Template, ViewError> {
         let txid = match mtid {
             None => return Err(ViewError::NotImplemented),
             Some(v) => hex::decode(&v)?,
         };
         let tx = get_transaction(&conn, txid.clone()).await?;
+        let is_blocked = cache.is_blocked(&txid);
+        let mut is_blocker = cookies.get("is_blocker").is_some();
+        if !is_blocked && is_blocker {
+            cookies.remove(Cookie::new("is_blocker", ""));
+            is_blocker = false;
+        }
         Ok(Template::render(
             "view-tx",
             &context! {
@@ -66,17 +105,99 @@ async fn view_transaction(conn: TransactionsDb, tid: Option<String>) -> Template
                 parent: "base",
                 menu_view_tx: true,
                 is_error: false, 
-                tx_id: hex::encode(txid),  
+                tx_id: hex::encode(txid.clone()),  
                 tx_title: tx.title, 
                 tx_description: tx.description,
                 tx_last: tx.history.last().unwrap().0.into_encoding(),
+                is_blocked,
+                is_blocker,
             },
         ))
     }
     
-    match view(conn, tid).await {
+    match view(conn, cache, cookies, tid).await {
         Ok(t) => t,
         Err(e) => render_error(&format!("{}", e)),
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BlockError {
+    #[error("Transaction id is not hex encoded")]
+    InvalidTxid(#[from] hex::FromHexError),
+    #[error("Transaction is already blocked")]
+    AlreadyBlocked,
+    #[error("{0}")]
+    DatabaseError(#[from] TxLoadError),
+}
+
+#[derive(Serialize)]
+struct BlockResp {
+    error: Option<String>,
+}
+
+#[post("/block/<txid>")]
+async fn block_transaction(conn: TransactionsDb, cache: &State<Cache>, cookies: &CookieJar<'_>, txid: String) -> Json<BlockResp> {
+    async fn block(conn: TransactionsDb, cache: &State<Cache>, tid: &str) -> Result<(), BlockError> {
+        let txid = hex::decode(tid)?;
+        let _ = get_transaction(&conn, txid.clone()).await?;
+        if cache.is_blocked(&txid) {
+            return Err(BlockError::AlreadyBlocked);
+        }
+        cache.block(&txid, Duration::minutes(5));
+        Ok(())
+    }
+    
+    match block(conn, cache, &txid).await {
+        Ok(_) => {
+            cookies.add(Cookie::new("is_blocker", ""));
+            Json(BlockResp {
+                error: None,
+            })
+        },
+        Err(e) => Json(BlockResp {
+            error: Some(format!("{}", e)),
+        }),
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum UnBlockError {
+    #[error("Transaction id is not hex encoded")]
+    InvalidTxid(#[from] hex::FromHexError),
+    #[error("Transaction is not blocked")]
+    NotBlocked,
+    #[error("{0}")]
+    DatabaseError(#[from] TxLoadError),
+}
+
+#[derive(Serialize)]
+struct UnBlockResp {
+    error: Option<String>,
+}
+
+#[post("/unblock/<txid>")]
+async fn unblock_transaction(conn: TransactionsDb, cache: &State<Cache>, cookies: &CookieJar<'_>, txid: String) -> Json<UnBlockResp> {
+    async fn unblock(conn: TransactionsDb, cache: &State<Cache>, tid: &str) -> Result<(), UnBlockError> {
+        let txid = hex::decode(tid)?;
+        let _ = get_transaction(&conn, txid.clone()).await?;
+        if !cache.is_blocked(&txid) {
+            return Err(UnBlockError::NotBlocked);
+        }
+        cache.unblock(&txid);
+        Ok(())
+    }
+    
+    match unblock(conn, cache, &txid).await {
+        Ok(_) => {
+            cookies.remove(Cookie::new("is_blocker", ""));
+            Json(UnBlockResp {
+                error: None,
+            })
+        },
+        Err(e) => Json(UnBlockResp {
+            error: Some(format!("{}", e)),
+        }),
     }
 }
 
@@ -164,9 +285,12 @@ fn rocket() -> _ {
                 index,
                 create_transaction,
                 post_transaction,
-                view_transaction
+                view_transaction,
+                block_transaction,
+                unblock_transaction,
             ],
         )
+        .manage(Cache::new())
         .attach(Template::fairing())
         .attach(TransactionsDb::fairing())
         .attach(AdHoc::on_ignite("Run Migrations", run_migrations))
